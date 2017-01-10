3b067ef2dba5a33d451fcecda5ca7cff6558f65a
Dennis Majvall <dennis.majvall.sp13@thegameassembly.com>
2017-01-10 19:03:30 +0100

    initial files

---
+++ src/app.js
@@ -0,0 +1,34 @@
+// globals
+m = {}; // all modules
+g = {}; // all global variables (ex. settings)
+
+// Require modules
+[
+  "express",
+  "compression",
+  "path",
+  "fs",
+  "serve-favicon",
+  "cookie-parser",
+  "body-parser",
+  "gulp",
+  "gulp-less",
+  "gulp-clean-css",
+  "./settingsConstr",
+  "./classLoader"
+].forEach(function(x){
+  // store required modules in "m"
+  m[x.replace(/\W/g,'')] = require(x);
+});
+
+// constructs g.settings object
+m.settingsConstr();
+
+// loads all classes
+m.classLoader();
+
+// start LessWatch
+new g.classes.LessWatch();
+
+// start express server
+new g.classes.Server();
