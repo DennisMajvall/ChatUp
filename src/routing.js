9895daf317a2fea3558f41218346b35a12bcfe46
Dennis Majvall <dennis.majvall.sp13@thegameassembly.com>
2017-01-11 18:46:22 +0100

    chat users now automatically gets all new messages sent to the server

--- src/routing.js
+++ src/routing.js
@@ -2,6 +2,8 @@
 
 var messages = [];
 var listeners = [];
+var timer = null;
+var listenerTimeLimit = 25000;
 
 var get_routes = {
 	'/read-messages/:lastTime': readMessagesRequest
@@ -11,20 +13,123 @@ var post_routes = {
 	'/send-message': sendMessageRequest
 };
 
-function updateListeners() {
-	res.json( messages );
+/*	-----------   Basic explanation   ----------
+		This comment may be removed later.
+
+	send message:
+		put msg in array of messages
+		send msg to everybody listening
+		remove all listeners
+
+	read message:
+		if the listener has new messages
+			send them
+			remove them from listeners
+		else
+			put listener in array of listeners
+			set timer countdown on the oldest listener + x seconds
+
+	timer countdown completed:
+		go through array of listeners
+		if any listener has been waiting for x seconds
+			send listener messages and remove listener
+
+		if there are any listeners left
+			set timer countdown on the oldest listener + x seconds
+*/
+
+function setNewTimeout() {
+	if (timer) {
+		clearTimeout(timer)
+		timer = null;
+	}
+
+	if (listeners.length) {
+		let timeToListen = Math.max(100, listeners[0].beginListeningTime + listenerTimeLimit - new Date().getTime());
+
+		timer = setTimeout(onTimeoutTick, timeToListen);
+	}
 }
 
+function onTimeoutTick() {
+	let hasUnreadMessages = true;
+
+	while (hasUnreadMessages && listeners.length) {
+		hasUnreadMessages = sendUnreadMessages(listeners[0]);
+
+		if (hasUnreadMessages) {
+			listeners = listeners.slice(1);
+		}
+	}
+
+	setNewTimeout();
+}
+
+function updateListeners() {
+	if (!listeners.length)
+		return;
+
+	listeners.sort(function(a, b) {
+		return a.latestMsgTime > b.latestMsgTime;
+	});
+
+	setNewTimeout();
+}
+
+// Returns true if unread messages were sent.
+function sendUnreadMessages(listener) {
+	let unreadMessages = [];
+
+	for (let i = messages.length - 1; i >= 0; --i) {
+		let msg = messages[i];
+
+		if (msg.timeReceived > listener.latestMsgTime) {
+			unreadMessages.push(msgToJSON(msg));
+		} else {
+			break;
+		}
+	}
+
+	let timeListened = new Date().getTime() - listener.beginListeningTime;
+
+	if (unreadMessages.length || timeListened > listenerTimeLimit) {
+		listener.res.json(unreadMessages.reverse());
+		return true;
+	}
+
+	return false;
+}
 
 function readMessagesRequest(req, res) {
-	res.json( messages );
-	return;
 	var listener = {
 		res: res,
-		latestMsg: req.params.lastTime
+		latestMsgTime: req.params.lastTime,
+		beginListeningTime: new Date().getTime()
 	};
-	listeners.push(listener);
-	updateListeners();
+
+	let hasUnreadMessages = sendUnreadMessages(listener);
+
+	if (!hasUnreadMessages) {
+		listeners.push(listener);
+		updateListeners();
+	}
+}
+
+function msgToJSON(msg) {
+	return {
+		text: msg.text,
+		time: msg.timeReceived
+	};
+}
+
+function sendMessageToAllListeners(msg) {
+	let jsonMsg = msgToJSON(msg);
+
+	for (let i = 0; i < listeners.length; ++i) {
+		listeners[i].res.json(jsonMsg);
+	}
+
+	listeners = [];
 }
 
 function sendMessageRequest(req, res) {
@@ -33,8 +138,10 @@ function sendMessageRequest(req, res) {
 		timeReceived: new Date().getTime()
 	}
 
+	res.json({ sendMessageRequest:"OK" });
+
 	messages.push(msg);
-	res.json({ok:"ok"});
+	sendMessageToAllListeners(msg);
 }
 
 module.exports = function(app) {
--- src/www/js/message-basics.js
+++ src/www/js/message-basics.js
@@ -1,19 +1,38 @@
 $(function() {
-	var sendButton = $('input[name="send-message"]').on('click', onSendMessage);
+	var sendButton = $('input[name="send-message"]');
 	var inputTextbox = $('input[name="input"]');
+	var lastReadMsgTime = 0;
+
+	waitForMessages();
+
+	sendButton.on('click', onSendMessage);
 
 	function onSendMessage() {
 		var inputMsg = inputTextbox.val();
 
-		$.post('/send-message', { 'text': inputMsg }, function(data, error) {
-			console.log(data);
-			onReadMessage();
+		$.post('/send-message',
+			{
+				'text': inputMsg
+				// we could send more parameters here.
+			},
+			function(data, error) {
 		}, 'json');
 	}
 
-	function onReadMessage() {
-		$.getJSON('/read-messages/lol', function(data) {
-			console.log(data[0]);
+	function onReceiveMessages(messages) {
+		console.log(data);
+
+		if (data.length) {
+			lastReadMsgTime = data[data.length-1].time;
+		}
+	}
+
+	function waitForMessages() {
+		$.getJSON('/read-messages/' + lastReadMsgTime, function(data) {
+			onReceiveMessages(data);
+
+			// Wait for new messages.
+			waitForMessages();
 		});
 	}
 });
