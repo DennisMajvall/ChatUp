48025eac7db2776966cf841e754658d36c4ac011
botanki <karzan@botani.nu>
2017-01-14 14:34:04 +0100

    timestamp @ messages

--- src/www/js/message-basics.js
+++ src/www/js/message-basics.js
@@ -45,8 +45,18 @@ $(function() {
             return '';
         }
 
-        return '<p>' + message.sender + '</p>';
+        return '<p>' + message.sender + ", " + formatTime(message.time) + '</p>';
     }
+
+    function formatTime(timestamp){
+	    var d = new Date(timestamp),
+	        time = [d.getHours(), d.getMinutes(), d.getSeconds()];
+
+	    time = time.map(function(t){
+	      return t < 10 ? '0' + t : t;
+	    });
+	    return time.join(':');
+	}
 
 	function waitForMessages() {
 		$.getJSON('/read-messages/' + lastReadMsgTime, function(messages) {
