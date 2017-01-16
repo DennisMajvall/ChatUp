df2ec9fa55de523395669a16e89ae2c991ccb568
botanki <karzan@botani.nu>
2017-01-16 14:53:11 +0100

    added cog-wheel as settings button and changed sizes on slide. also added glyphicons to fonts

--- src/www/js/left-slide.js
+++ src/www/js/left-slide.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
     $('.left-panel').on('mouseenter', function(e) {
-        console.log('hej');
         $('.left-panel').addClass('expanded');
         $('.chat-area').addClass('decreased');
         $('.right-panel').addClass('decreased');
     });
 
     $('.left-panel').on('mouseleave', function(e) {
-        console.log('hejdå');
-            $('.left-panel').removeClass('expanded');    
-            $('.chat-area').removeClass('decreased');
-            $('.right-panel').removeClass('decreased');
+        $('.left-panel').removeClass('expanded');    
+        $('.chat-area').removeClass('decreased');
+        $('.right-panel').removeClass('decreased');
     });
 });
--- src/www/js/right-slide.js
+++ src/www/js/right-slide.js
@@ -1,19 +1,13 @@
 $(document).ready(function() {
-        console.log('abc');
     $('.right-panel').on('mouseenter', function(e) {
-        console.log('hej');
         $('.right-panel').addClass('expanded');
         $('.chat-area').addClass('decreased');
         $('.left-panel').addClass('decreased');
     });
 
     $('.right-panel').on('mouseleave', function(e) {
-        //setTimeout(function() {
-        console.log('hejdå');
-            $('.right-panel').removeClass('expanded');    
-            $('.chat-area').removeClass('decreased');
-            $('.left-panel').removeClass('decreased');
-
-        //}, 300);
+        $('.right-panel').removeClass('expanded');    
+        $('.chat-area').removeClass('decreased');
+        $('.left-panel').removeClass('decreased');
     });
 });
